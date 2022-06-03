eff48963894503e879834ac60720995ab5066451
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2022-06-03 17:46:27 +0400

    response

---
+++ schemas/get_limited_promo_response.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, HttpUrl
+from typing import Optional
+from datetime import datetime
+from enum import Enum
+
+
+class CathegoryEnum(str, Enum):
+    sushi = 'sushi'
+    pizza = 'pizza'
+    kebab = 'kebab'
+    fast_food = 'fast_food'
+
+
+class GetLimitedPromoResponse(BaseModel):
+    item_id: int
+    created_at: datetime
+    updated_at: datetime
+    title: str
+    old_price: Optional[str]
+    new_price: str
+    cathegory: CathegoryEnum
+    weight: Optional[str]
+    img: Optional[HttpUrl]
+    website_link: HttpUrl
+    website_title: str
+    link: HttpUrl
+    phone_number: str
+
+    class Config:
+        orm_mode = True
