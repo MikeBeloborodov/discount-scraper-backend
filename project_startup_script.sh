44fce7cba526f95c32d176c1396c65b65977a0c9
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2022-06-13 19:19:28 +0400

    added startup script

---
+++ project_startup_script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Run command in the background
+_evalBg(){
+	eval "$@" &>/dev/null & disown;
+}
+# Commands
+backend_cmd="uvicorn routes.main:app --reload";
+frontend_cmd="npm start"
+
+# Backend startup
+echo "Starting backend server ...";
+cd ./backend;
+_evalBg "${backend_cmd}";
+echo "Backend startup complete.";
+
+# Frontend startup
+echo " ";
+echo Starting frontend server ...
+cd ..
+cd ./frontend/discount-frontend
+_evalBg "${frontend_cmd}"
+echo Frontend server started.
+
+# Exit
+cd ..
+cd ..
+echo " ";
+echo Script exited with no erorrs.
